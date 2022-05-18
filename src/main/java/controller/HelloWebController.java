fb695dcc8059a8663e5b5ceb7d39cba24fbb1a5d
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-18 10:45:36 +0530

    Add to Display hello in web page

---
+++ src/main/java/controller/HelloWebController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HelloWebController
+{
+    @GetMapping("/web")
+    public String hello(){
+        return "hello";
+    }
+    @GetMapping("/web/message")
+    public String message(Model model){
+        model.addAttribute("message", "This is a custom message");
+        return "message";
+
+    }
+}
